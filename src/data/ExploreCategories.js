7057ed4cb524f93e43e1909af63b8f6b95db9586
Sandeep Singh <snayal50@gmail.com>
2025-02-25 12:32:22 +0530

    complete categories

---
+++ src/data/ExploreCategories.js
@@ -0,0 +1,44 @@
+export const exploreCategories = [
+  {
+    id: 1,
+    category: "Beauty",
+    image:
+      "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/1.png",
+    highlight: "Glow Up with the Best Beauty Picks!",
+  },
+  {
+    id: 2,
+    category: "groceries",
+    image:
+      "https://cdn.dummyjson.com/products/images/groceries/Green%20Bell%20Pepper/1.png",
+    highlight: "Fresh & Organic – Delivered to Your Doorstep!",
+  },
+  {
+    id: 3,
+    category: "home-decoration",
+    image:
+      "https://cdn.dummyjson.com/products/images/home-decoration/Decoration%20Swing/thumbnail.png",
+    highlight: "Transform Your Space with Elegant Decor!",
+  },
+  {
+    id: 4,
+    category: "mobile-accessories",
+    image:
+      "https://cdn.dummyjson.com/products/images/mobile-accessories/TV%20Studio%20Camera%20Pedestal/thumbnail.png",
+    highlight: "Upgrade Your Gadgets with Premium Accessories!",
+  },
+  {
+    id: 5,
+    category: "smartphones",
+    image:
+      "https://cdn.dummyjson.com/products/images/smartphones/Oppo%20F19%20Pro%20Plus/thumbnail.png",
+    highlight: "Latest Smartphones – Power in Your Hands!",
+  },
+  {
+    id: 6,
+    category: "sports-accessories",
+    image:
+      "https://cdn.dummyjson.com/products/images/sports-accessories/Tennis%20Racket/thumbnail.png",
+    highlight: "Gear Up & Stay Active with Top Sports Accessories!",
+  },
+];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import ElectronicsProducts from "../components/home/ElectronicsProducts"
 import HeroSlider from "../components/home/HeroSlider"
 import PopularProducts from "../components/home/PopularProducts"
+import ExploreCategories from "../components/home/ExploreCategories"
+import FeaturedProducts from "../components/home/FeaturedProducts"
 
 
 const HomePage = () => {
@@ -9,6 +11,8 @@ const HomePage = () => {
       <HeroSlider />
       <PopularProducts />
       <ElectronicsProducts />
+      <ExploreCategories />
+      <FeaturedProducts />
     </section>
   )
 }
