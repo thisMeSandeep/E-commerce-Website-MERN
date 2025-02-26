e1d1843ae66c7aa867d5ebd4af33f657b9b272e3
Sandeep Singh <snayal50@gmail.com>
2025-02-26 11:49:16 +0530

    update breadcrumps

--- src/components/commonComponents/BreadCrumbs.jsx
+++ src/components/commonComponents/BreadCrumbs.jsx
@@ -1,11 +1,12 @@
 import { ChevronRight, Home } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const BreadCrumbs = ({title}) => {
     return (
         <div className="flex text-gray-500 text-sm items-center gap-2 py-4 bg-blue-100/50 px-5 overflow-x-scroll no-srollbar text-nowrap">
-            <span className="flex items-center gap-2"><Home className="size-5" /> Home</span>
+            <Link to="/" className="flex items-center gap-2"><Home className="size-5 text-orange-500" /> Home</Link>
             <ChevronRight className="size-5" />
-            <span>Shop</span>
+            <Link to="/products">Shop</Link>
             <ChevronRight className="size-5" />
             <span>All Categories</span>
             <ChevronRight className="size-5" />
